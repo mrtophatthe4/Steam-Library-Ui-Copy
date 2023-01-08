5260f163770a2f6e37a4d06f504493f3bb82475e
mrtophatthe4 <mrtophatthe4@gmail.com>
2023-01-08 10:41:20 -0500

    WOOP ALMOST FINISHED!! (with the library part)
    www

--- scripts/library.js
+++ scripts/library.js
@@ -1,25 +1,54 @@
 const searchGameInput = document.getElementById('searchGame');
 const scrapSearchIcon = document.getElementById('scrapSearchIcon');
-var isXEnabeled = false
+const gameImg = document.getElementById("GameMenuImg");
+const gameLogo = document.getElementById("GameMenuLogo");
+var activeButton = "10";
+/* const popupblur = document.getElementById("popupblur");
+const popup = document.getElementById("popup"); */
 
-scrapSearchIcon.style.visibility = "hidden"
-alert("gay")
+var isXEnabeled = false;
+
+scrapSearchIcon.style.visibility = "hidden";
 
 function findGame(game) {
-    alert(["game index: ", game])
+    switch(game) {
+        case 10:
+            gameImg.src = "assets/gameImages/10_library_hero.jpg";
+            gameLogo.src = "assets/gameImages/10_logo.png";
+            break;
+        case 70:
+            gameImg.src = "assets/gameImages/70_library_hero.jpg";
+            gameLogo.src = "assets/gameImages/70_logo.png";
+            break;
+        default:
+        
+      }
+      setActiveButton(game);
 }
 
+function setActiveButton(active) {
+    document.getElementById(active).className = "GameSelected";
+    document.getElementById(activeButton).className = "Game";
+    activeButton = active;
+}
 searchGameInput.addEventListener('input',  event => {
     var searchvar = searchGameInput.value
     if (searchvar === "") {
-        var isXEnabeled = false
-        scrapSearchIcon.style.visibility = "hidden"
+        var isXEnabeled = false;
+        scrapSearchIcon.style.visibility = "hidden";
     } else {
         if(isXEnabeled === false){
-            var isXEnabeled = true
-            scrapSearchIcon.style.visibility = "visible"
-            alert(isXEnabeled)
+            var isXEnabeled = true;
+            scrapSearchIcon.style.visibility = "visible";
+            alert(isXEnabeled);
         }
     }
-    alert(["this has not been added yet lol come back later", searchvar, "<--"])
+    alert(["this has not been added yet lol come back later", searchvar, "<--"]);
 });
+
+/* function ContinueToWebsite() {
+    popup.style.visibility = "hidden"
+    popupblur.style.visibility = "hidden"
+    popup.remove()
+    popupblur.remove()
+} */
